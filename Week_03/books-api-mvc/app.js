aac0f91894c0c7e60e52bc52c7adeb19dfc5617f
rottenpastaz <johnyonghaiwen@gmail.com>
2024-05-12 22:46:03 +0800

    WEEK 4

--- Week_03/books-api-mvc/app.js
+++ Week_03/books-api-mvc/app.js
@@ -1,12 +1,20 @@
-class Book {
-    constructor(id, title, author) {
-      this.id = id;
-      this.title = title;
-      this.author = author;
-    }
-  
-    // ... methods for CRUD operations (explained later)
-  }
-  
-  module.exports = Book;
-  
+const express = require("express");
+const bodyParser = require("body-parser");
+const booksController = require("./controllers/booksController"); // Import controllers
+const app = express();
+app.use(bodyParser.json()); // Parse incoming JSON data in request body
+app.use(bodyParser.urlencoded({ extended: true })); // For form data handling
+// Define individual routes for each controller function
+app.get("/books", booksController.getAllBooks);
+app.get("/books/:id", booksController.getBookById);
+app.post("/books", booksController.createBook);
+app.put("/books/:id", booksController.updateBook);
+app.delete("/books/:id", booksController.deleteBook);
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+const validateBook = require("./middlewares/validateBook");
+app.post("/books", validateBook, booksController.createBook); // Add validateBook before createBook
+app.put("/books/:id", validateBook, booksController.updateBook); // Add validateBook before updateBook
--- Week_03/books-api-mvc/controllers/booksController.js
+++ Week_03/books-api-mvc/controllers/booksController.js
@@ -1,6 +1,4 @@
 const Book = require("../models/book");
-const Book = require("../models/book");
-
 const getAllBooks = async (req, res) => {
   try {
     const books = await Book.getAllBooks();
@@ -74,24 +72,3 @@ module.exports = {
   updateBook,
   deleteBook,
 };
-
-const express = require("express");
-const bodyParser = require("body-parser");
-const booksController = require("./controllers/booksController"); // Import controllers
-
-const app = express();
-app.use(bodyParser.json()); // Parse incoming JSON data in request body
-app.use(bodyParser.urlencoded({ extended: true })); // For form data handling
-
-// Define individual routes for each controller function
-app.get("/books", booksController.getAllBooks);
-app.get("/books/:id", booksController.getBookById);
-app.post("/books", booksController.createBook);
-app.put("/books/:id", booksController.updateBook);
-app.delete("/books/:id", booksController.deleteBook);
-
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
